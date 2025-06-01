c8525b4f5d4666257854a055865851663160fff8
Luisen Ramos <luisenramos0805@gmail.com>
2025-05-31 19:45:43 -0500

    Read Variables and turn them into XML instructions

--- Xmlmaker.py
+++ Xmlmaker.py
@@ -1,4 +1,107 @@
 import xml.etree.ElementTree as ET
+import random
+import string
+import re
+
+def generate_open_roberta_id(length=20):
+    characters = string.ascii_letters + string.digits + "!@#$%^&*()-_=+[]{}|;:',.<>?/"
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def findVar(line):
+    global variables_declared  # Accede a la variable global
+    pattern = r"^var\s+(\w+)\s*=\s*(.+)"
+    match = re.search(pattern, line)
+    
+    if match:
+        variable_name = match.group(1)
+        variable_value = match.group(2)
+        
+        # Verifica si el nodo 'variables' necesita ser creado
+        if not variables_declared:
+            variables = ET.SubElement(block_start, "statement", {"name": "ST"})
+            variables_declared = True
+            block_start.find("mutation").set("declare", "true")
+        else:
+            # Recupera la referencia al nodo 'statement' ya existente
+            variables = block_start.find("statement[@name='ST']")
+        
+        # Crear un bloque para la declaración de la variable
+        variable_block = ET.SubElement(variables, "block", {
+            "type": "robGlobalVariables_declare",
+            "id": generate_open_roberta_id(),
+            "intask": "true",
+            "deletable": "false"
+        })
+
+        # Identificar el tipo de variable y configurar los nodos hijos
+        if variable_value.isdigit():
+            mutation = ET.SubElement(variable_block, "mutation", {
+                "next": "true",
+                "declaration_type": "Number"
+            })
+            field_type = "Number"
+            block_type = "math_integer"
+            field_name = "NUM"
+        
+        elif variable_value.lower() in ("true", "false"):
+            mutation = ET.SubElement(variable_block, "mutation", {
+                "next": "true",
+                "declaration_type": "Boolean"
+            })
+            field_type = "Boolean"
+            block_type = "logic_boolean"
+            field_name = "BOOL"
+        
+        elif variable_value == "[]":
+            mutation = ET.SubElement(variable_block, "mutation", {
+                "next": "true",
+                "declaration_type": "Array_Number"
+            })
+            field_type = "Array_Number"
+            block_type = "robLists_create_with"
+            field_name = "LIST_TYPE"
+
+        else:
+            # Manejo de valores desconocidos como cadenas de texto
+            mutation = ET.SubElement(variable_block, "mutation", {
+                "next": "true",
+                "list_type": "Number"
+            })
+            field_type = "String"
+            block_type = "text"
+            field_name = "TEXT"
+
+        # Configuración de nodos comunes
+        field_var = ET.SubElement(variable_block, "field", {"name": "VAR"})
+        field_type_field = ET.SubElement(variable_block, "field", {"name": "TYPE"})
+        field_var.text = variable_name
+        field_type_field.text = field_type
+        
+        value = ET.SubElement(variable_block, "value", {"name": "VALUE"})
+        block_value = ET.SubElement(value, "block", {
+            "type": block_type,
+            "id": generate_open_roberta_id(),
+            "intask": "true"
+        })
+        
+        if block_type == "math_integer":
+            field_value = ET.SubElement(block_value, "field", {"name": field_name})
+            field_value.text = variable_value
+        elif block_type == "logic_boolean":
+            field_value = ET.SubElement(block_value, "field", {"name": field_name})
+            field_value.text = variable_value.upper()
+        elif block_type == "robLists_create_with":
+            mutation_items = ET.SubElement(block_value, "mutation", {
+                "items": "0",
+                "list_type": "Number"
+            })
+            field_list_type = ET.SubElement(block_value, "field", {"name": field_name})
+            field_list_type.text = "Number"
+        elif block_type == "text":
+            field_value = ET.SubElement(block_value, "field", {"name": field_name})
+            field_value.text = variable_value
+
+
 
 # Crear el elemento raíz con el namespace
 namespace = "http://de.fhg.iais.roberta.blockly"
@@ -26,17 +129,17 @@ instance_program = ET.SubElement(block_set_program, "instance", {
 # Agregar un bloque a la instancia
 block_start = ET.SubElement(instance_program, "block", {
     "type": "robControls_start",
-    "id": "%bFKt:1`BRJ|bm,R8v=E",
+    "id": generate_open_roberta_id(),
     "intask": "true",
     "deletable": "false"
+
 })
 
 # Agregar los elementos secundarios del bloque
+variables_declared = False
 mutation = ET.SubElement(block_start, "mutation", {"declare": "false"})
 field_debug = ET.SubElement(block_start, "field", {"name": "DEBUG"})
 field_debug.text = "TRUE"
-
-
 
 # Crear 'config'
 config = ET.SubElement(root, "config")
@@ -63,6 +166,15 @@ block_brick = ET.SubElement(instance_config, "block", {
     "intask": "true",
     "deletable": "false"
 })
+
+f = open("Code.txt", "r")
+
+while True:
+    line = f.readline()
+    if(len(line) == 0):
+        break
+    findVar(line)
+f.close()
 
 # Escribir el árbol XML a un archivo sin la declaración
 tree = ET.ElementTree(root)
